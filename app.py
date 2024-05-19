7a05ba6bf217a0d9c8435d0b0d18f3b479dd1b6b
Vishnu <vishnuraghav16@gmail.com>
2024-05-19 15:34:13 +0400

    empty route handle

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask import Flask, jsonify
 import gdown
 app = Flask(__name__)
 
-# Ignore favicon requests
-@app.route('/favicon.ico')
-def favicon():
-    return '', 404
+
+# Route to indicate that the server is running
+@app.route('/')
+def server_running():
+    return jsonify(message='Server is running')
 
 @app.route('/run-colab')
 def run_colab():
     gdown.download('https://drive.google.com/file/d/1iGLQAtupTFPTPhtdWVg3Q5ECR7gR1cYY', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
+
+# Ignore favicon requests
+@app.route('/favicon.ico')
+def favicon():
+    return '', 404
